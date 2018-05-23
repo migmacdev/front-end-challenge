bebbb9b5623c4f341887554fd96981e2fc3b6e6f
Miguel Maciel <iscmiguelmaciel@outlook.com>
2018-05-23 15:55:33 -0500

    Add initial files, setup npm dependencies and gitignore

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ExchangePage from './components/ExchangePage'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
