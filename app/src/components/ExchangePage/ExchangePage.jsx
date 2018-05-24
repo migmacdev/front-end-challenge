5bd8eaf59f7d9b2ab9fda42c164f344d0512867f
Miguel Maciel <iscmiguelmaciel@outlook.com>
2018-05-24 10:37:22 -0500

    Refactor Navigationbar and added to the ExchangePage

--- app/src/components/ExchangePage/ExchangePage.jsx
+++ app/src/components/ExchangePage/ExchangePage.jsx
@@ -1,51 +1,14 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
-import { NavItem } from 'react-bootstrap';
-import { NavDropdown } from 'react-bootstrap';
-import { MenuItem } from 'react-bootstrap';
-import brand from '../../sharedfiles/images/bitso_logo@2x.png';
+import NavigationBar from '../NavigationBar/';
 
-
+/*
+*	ExhangePage, defines the structure of the page panels
+*/
 class ExchangePage extends React.Component{
 	
 	render(){
 		return (
-			<Navbar>
-  			<Navbar.Header>
-			    <Navbar.Brand>
-			      	<img src = {brand}/>
-			    </Navbar.Brand>
-			  	</Navbar.Header>
-			  	<Nav>
-			    	<Navbar.Text>
-			      		|
-			    	</Navbar.Text>
-			     	<Navbar.Text>
-			      		EXCHANGE
-			    	</Navbar.Text>
-				</Nav>
-				<Nav pullRight>
-			    	<NavItem eventKey={1} href="#" >
-			      		1 BTC  = 000,000 MXN
-			    	</NavItem>
-			    	<NavItem eventKey={1} href="#" >
-			    		|
-			    	</NavItem>
-			    	<NavItem eventKey={1} href="#" >
-			    		Wallet
-			    	</NavItem>
-			    	<NavItem eventKey={1} href="#" >
-			    		Exchange
-			    	</NavItem>
-			    	<NavItem eventKey={1} href="#" >
-			    		Ayuda
-			    	</NavItem>
-			    	<NavItem eventKey={1} href="#" >
-			    		Usuario
-			    	</NavItem>
-				</Nav>
-			</Navbar>
+			<NavigationBar />
 		);
 	}
 }
---
+++ app/src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,61 @@
+
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
+import brand from '../../sharedfiles/images/bitso_logo@2x.png';
+import dropdown from '../../sharedfiles/images/icon_dropdown@2x.png';
+
+
+/*
+*	NavigationBar using responsive bootstrap components
+*/
+
+class NavigationBar extends React.Component{
+	
+	render(){
+		return (
+			<Navbar collapseOnSelect>
+				{/*Nav Brand*/}
+	  			<Navbar.Header>
+				    <Navbar.Brand>
+				      	<img src = {brand}/>
+				    </Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+
+				{/*NavItems*/}
+				<Navbar.Collapse>
+				  	<Nav pullLeft>
+				     	<Navbar.Text>
+				      		EXCHANGE
+				    	</Navbar.Text>
+					</Nav>
+					<Nav pullRight>
+				    	<NavItem eventKey={1} href="#" >
+				      		1 BTC  = 000,000 MXN
+				    	</NavItem>
+				    	<NavItem eventKey={1} href="#" >
+				    		Wallet   <img src = {dropdown}/>
+				    	</NavItem>
+				    	<NavItem eventKey={1} href="#" >
+				    		Exchange <img src = {dropdown}/>
+				    	</NavItem>
+				    </Nav>
+			    	<Nav pullRight>
+				    	<NavItem eventKey={1} href="#" >
+				    		Ayuda
+				    	</NavItem>
+				    	<NavItem eventKey={1} href="#" >
+				    		Usuario
+				    	</NavItem>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
+}
+
+export default NavigationBar;
