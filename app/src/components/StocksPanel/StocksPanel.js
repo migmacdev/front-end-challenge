761a84b06b3e5380ac83eb7cde2c7bcf6061a595
miguelmaciel <iscmiguelmaciel@outlook.com>
2018-05-29 08:48:55 -0500

    Add, charts library, initial candle chart set up

--- app/src/components/StocksPanel/StocksPanel.js
+++ app/src/components/StocksPanel/StocksPanel.js
@@ -7,6 +7,31 @@ import {ButtonToolbar} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import {DropdownButton} from 'react-bootstrap';
 import {MenuItem} from 'react-bootstrap';
+
+
+import Chart from './CandleChart';
+import { getData } from "./utils"
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
+
+class ChartComponent extends React.Component {
+	componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+	}
+	render() {
+		if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		return (
+			<TypeChooser>
+				{type => <Chart type={type} width = {600} data={this.state.data} />}
+			</TypeChooser>
+		)
+	}
+}
+
 /*
 *	LastTradesPanel using responsive bootstrap components
 */
@@ -55,4 +80,4 @@ class StocksPanel extends React.Component{
 	}
 }
 
-export default StocksPanel;
+export default ChartComponent;
