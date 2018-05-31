dd6b4f5241a2dbd54a74ff7e6cc752642461ea90
miguelmaciel <iscmiguelmaciel@outlook.com>
2018-05-30 19:59:29 -0500

    Add, chart on every markets chart

--- app/src/components/MarketsSidePanel/MarketChartItem.js
+++ app/src/components/MarketsSidePanel/MarketChartItem.js
@@ -5,13 +5,13 @@ import {Panel, Button} from 'react-bootstrap';
 import {LineChart, Line} from 'recharts';
 
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page A', pv: 1000},
+      {name: 'Page B', pv: 5000},
+      {name: 'Page C', pv: 1500},
+      {name: 'Page D', pv: 2000},
+      {name: 'Page E', pv: 4000},
+      {name: 'Page F', pv: 10000},
+      {name: 'Page G', pv: 400},
 ];
 
 
@@ -45,14 +45,14 @@ class MarketChartItem extends React.Component{
 	render(){
 		return(
 			<li key = {this.props.book.book}>
-				<Button onClick={() =>{this.onPanelClick()}
+				<div onClick={() =>{this.onPanelClick()}
 				}>
 					<div>
-						<h4>{this.props.book.book}</h4>
+						<h6>{this.props.book.book}</h6>
 						<h6>{this.props.book.change}</h6>
-						<h4>{this.props.book.value}</h4>
+						<h6>{this.props.book.value}</h6>
 					</div>
-		        </Button>
+		        </div>
 		        <br />
 		        <Panel id="collapsible-panel-example-1" expanded={this.state.open}>
 					<Panel.Collapse>
